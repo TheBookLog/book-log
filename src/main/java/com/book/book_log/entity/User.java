86077c39c9a799d7550b35fe09d0b05520af3bb8
dani <danii0110@naver.com>
2025-01-15 00:25:36 +0900

    feature/back/kakao-login; User 엔티티 수정 및 OAuthProvider 추가
    - User 엔티티의 소셜 로그인 관련 필드 활성화
    	- 'oauth_provider', 'oauth_token', 'refresh_token', 'expires_at' 필드 추가
    	- 필드에 Validation 어노테이션 적용(@NotNull, @Size)
    	- 긴 문자열 처리를 위해 @Lob 어노테이션 추가
    - OAuthProvider Enum 생성
    	- KAKAO, NAVER, GOOGLE, GITHUB 값 추가
    	- 데이터 무결성을 보장하고 소셜 로그인 제공자 관리 용이

--- src/main/java/com/book/book_log/entity/User.java
+++ src/main/java/com/book/book_log/entity/User.java
@@ -1,6 +1,8 @@
 package com.book.book_log.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,6 +23,8 @@ public class User {
     private String id;
 
     @Column(length = 50, nullable = false)
+    @NotNull(message = "Username cannot be null")
+    @Size(max = 50, message = "Username must be less than 50 characters")
     private String username; // 설정하지 않을 경우 소셜 로그인 ID를 기본값으로 설정
 
     @Enumerated(EnumType.STRING)
@@ -31,20 +35,24 @@ public class User {
     @Column(length = 20)
     private AgeGroup age_group;
 
-//    // 소셜 로그인 관련 필드(나중에 구현)
-//    @Column(length = 20, nullable = false)
-//    private String oauth_provider;
-//
-//    @Lob
-//    @Column(nullable = false)
-//    private String oauth_token;
-//
-//    @Lob
-//    @Column(nullable = false)
-//    private String refresh_token;
-//
-//    @Column(nullable = false)
-//    private LocalDateTime expires_at;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    @NotNull(message = "OAuth provider cannot be null")
+    private OAuthProvider oauth_provider;
+
+    @Lob
+    @Column(nullable = false)
+    @NotNull(message = "Oauth token cannot be null")
+    private String oauth_token;
+
+    @Lob
+    @Column(nullable = false)
+    @NotNull(message = "Refresh token cannot be null")
+    private String refresh_token;
+
+    @Column(nullable = false)
+    @NotNull(message = "Token expiration date cannot be null")
+    private LocalDateTime expires_at;
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
