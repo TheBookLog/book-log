7a30d6f9a023ea5069d55eb9cb9b155cfde16c74
dani <danii0110@naver.com>
2025-08-24 22:30:33 +0900

    refactor; Category 엔티티 수정

--- src/main/java/com/book/book_log/entity/Category.java
+++ src/main/java/com/book/book_log/entity/Category.java
@@ -4,26 +4,26 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "categories")
 @Getter
 @Setter
 @NoArgsConstructor
-@Entity
-@Table(name = "category")
 public class Category {
-    @Id
-    private Integer id; // 알라딘에서 제공하는 categoryID 그대로 사용
 
-    @Column(nullable = false, length = 255)
-    private String name; // 카테고리명
+    @Id
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
-    private Category parent; // 상위 카테고리(nullable)
+    private Category parent;
 
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
-    private List<Category> subCategories = new ArrayList<>(); // 하위 카테고리 목록
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> subCategories = new ArrayList<>();
 }
